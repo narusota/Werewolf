2c1288d245fe67b3ae474b5abb9c390cafa17723
narusota <soutanarushima@gmail.com>
2026-05-09 15:21:50 +0900

    rename index.html to game.html and add dynamic player setup

--- main.js
+++ main.js
@@ -1,16 +1,22 @@
-let player = {
-    1: true,
-    2: true,
-    3: true,
-    4: false
-};
+let player = {};
+const playerSetBtn = document.querySelector("#playerSetBtn");
+let playerSet = document.querySelector("#playerSet");
+
+playerSetBtn.addEventListener("click", function () {
+    const count = parseInt(playerSet.value);
+
+    for (let i = 1; i <= count; i++) {
+        player[i] = "";
+    }
+    playerSet.value = "";
+})
 
 const nameInput = document.querySelector("#nameInput");
 const searchBtn = document.querySelector("#searchBtn");
 
-searchBtn.addEventListener("click", function() {
+searchBtn.addEventListener("click", function () {
     if (nameInput.value in player) {
-        console.log("キーがあります");
+        console.log(player[nameInput.value]);
     } else {
         console.log("キーがありません");
     }
