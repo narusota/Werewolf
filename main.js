2ce9640e02e74ab539d5080e2f094f40aa28d4eb
narusota <soutanarushima@gmail.com>
2026-05-10 14:15:48 +0900

    add player count validation (4-16)

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
 let player = {};
 const playerSetBtn = document.querySelector("#playerSetBtn");
 const playerSet = document.querySelector("#playerSet");
-
+const errorMsg = document.querySelector("#errorMsg");
 
 playerSetBtn.addEventListener("click", function () {
     const count = parseInt(playerSet.value);
+
+    if (count < 4 || 16 < count) {
+        errorMsg.textContent = "4~16人で設定してください"
+        return;
+    }
+
+    errorMsg.textContent = "";
+
     const ramdom = Math.floor(Math.random() * count) + 1;
 
     for (let i = 1; i <= count; i++) {
