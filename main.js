c6858bc5fa7618569924144c2aa746915573d02b
narusota <soutanarushima@gmail.com>
2026-05-10 13:54:23 +0900

    add random werewolf assignment to player

--- main.js
+++ main.js
@@ -2,12 +2,18 @@ let player = {};
 const playerSetBtn = document.querySelector("#playerSetBtn");
 const playerSet = document.querySelector("#playerSet");
 
+
 playerSetBtn.addEventListener("click", function () {
     const count = parseInt(playerSet.value);
+    const ramdom = Math.floor(Math.random() * count) + 1;
 
     for (let i = 1; i <= count; i++) {
         player[i] = true;
     }
+
+    player[ramdom] = false;
+    console.log(`${ramdom}にfalseが入りました`);
+           
     playerSet.value = "";
 })
 
